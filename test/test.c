3e2d9ff1f812ffbb1c39273e43fd2ca6299e2929
Ayham133 <ayhamatallah52@gmail.com>
2026-07-18 12:16:53 +0300

    add h file

---
+++ test/test.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    printf("Testing.........");
+    assert(true);
+    printf("Passed\n");
+    return EXIT_SUCCESS;
+}
