419497094d3ea49155fe05b4c4158d404ed727a7
Ayham133 <ayhamatallah52@gmail.com>
2026-07-23 12:45:11 +0300

    updated the CMakeLists.txt file, and added the raylib lib into the project

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "raylib.h"
+
 int main(void)
 {
     printf("Hello from, linked_list_visualizer!\n");
+
+    InitWindow(800, 1000, "Linked List Visualizer");
+
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return EXIT_SUCCESS;
 }
--- test/test.h
+++ test/test.h
@@ -2,17 +2,17 @@
 #define TEST_H
 
 // Regular text
-#define BLK "\e[0;30m"
-#define RED "\e[0;31m"
-#define GRN "\e[0;32m"
-#define YEL "\e[0;33m"
-#define BLU "\e[0;34m"
-#define MAG "\e[0;35m"
-#define CYN "\e[0;36m"
-#define WHT "\e[0;37m"
+#define BLK "\033[0;30m"
+#define RED "\033[0;31m"
+#define GRN "\033[0;32m"
+#define YEL "\033[0;33m"
+#define BLU "\033[0;34m"
+#define MAG "\033[0;35m"
+#define CYN "\033[0;36m"
+#define WHT "\033[0;37m"
 
 // Reset
-#define RESET "\e[0m"
+#define RESET "\033[0m"
 
 // Test Macro
 #define RUN_TEST(test)                      \
